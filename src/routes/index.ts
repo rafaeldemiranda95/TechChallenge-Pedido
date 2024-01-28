ed2d8844db38f4e560450d14b170a4bb34a4f6d7
Rafael de Miranda <37124918+rafaeldemiranda95@users.noreply.github.com>
2024-01-28 19:49:08 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,12 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.status(200).send('OK');
 });
+router.get('/test', (req, res) => {
+  res.status(200).send('OK');
+});
+router.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
 
 
 export default router;
